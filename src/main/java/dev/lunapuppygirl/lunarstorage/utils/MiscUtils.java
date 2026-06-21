2e160e0c79ebb09409a8c1c7278f5dd1e2473a27
lunapuppy <lunapuppygirl@proton.me>
2026-06-21 14:14:26 +0200

    feat: Format ms to date

--- src/main/java/dev/lunapuppygirl/lunarstorage/utils/MiscUtils.java
+++ src/main/java/dev/lunapuppygirl/lunarstorage/utils/MiscUtils.java
@@ -2,7 +2,14 @@ package dev.lunapuppygirl.lunarstorage.utils;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public class MiscUtils {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
     public static String getIp(HttpServletRequest request) {
         String ip = request.getHeader("X-Forwarded-For");
         if (ip != null && !ip.isBlank()) {
@@ -11,5 +18,14 @@ public class MiscUtils {
             ip = request.getRemoteAddr();
         }
         return ip;
+    }
+
+    public static String formatDate(long millis) {
+        LocalDateTime dateTime = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(millis),
+                ZoneId.systemDefault()
+        );
+
+        return dateTime.format(FORMATTER);
     }
 }
